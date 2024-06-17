733be43828605a3361d4d33f8bc4c4aec79c342c
haarry9 <hspv99@gmail.com>
2024-06-17 19:16:20 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-#textSummarizerNLPApp is the local package installed in the setup.py
+from textSummarizerNLPApp.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizerNLPApp.logging import logger
 
-
-logger.info("Welcome to our custom logging")
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx===================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
